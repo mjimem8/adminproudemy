daa7b7c5658593895edefa29577b58f1b648ad46
mjimem8 <4273880-mjimem8@user.noreply.gitlab.com>
2019-11-04 20:19:28 +0100

    primer commit

---
+++ src/app/shared/breadcrubs/breadcrubs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-breadcrubs',
+  templateUrl: './breadcrubs.component.html',
+  styles: []
+})
+export class BreadcrubsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
